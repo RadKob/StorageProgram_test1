bb247a12719aa156756c433c3b3cfbbbe39cfd53
radek <radek109@o2.pl>
2022-01-19 23:12:10 +0100

    Dodaj pliki projektów.

---
+++ ProgramInwentaryzacyjny/Data/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramInwentaryzacyjny.Data
+{
+    class Product
+    {
+        public string Symbol { get; set; }
+    }
+}
---
+++ ProgramInwentaryzacyjny/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+using System.Data.SQLite;
+using System.Windows;
+
+namespace ProgramInwentaryzacyjny
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly string connection_string = "Data Source=BazaDoProgramu.db;Version=3;New=false;Compress=True;";
+        private SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        private SQLiteDataAdapter dataAdapter;
+        DataTable dt = new DataTable();
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadProducts();
+        }
+        private void ConnectToDatabase()
+        {
+            sql_con = new SQLiteConnection(connection_string);
+            sql_con.Open();
+        }
+        private void CloseConnection()
+        {
+            sql_con.Close();
+        }
+        private void ExecuteQuery(string txtQuery)
+        {
+            ConnectToDatabase();
+            sql_cmd = sql_con.CreateCommand();
+            sql_cmd.CommandText = txtQuery;
+            sql_cmd.ExecuteNonQuery();
+            CloseConnection();
+        }
+        private void LoadProducts()
+        {
+            string txtQuery = "Select * from Products";
+            ConnectToDatabase();
+            sql_cmd = sql_con.CreateCommand();
+            dataAdapter = new SQLiteDataAdapter(txtQuery, sql_con);
+            dt = new DataTable("Products");
+            dataAdapter.Fill(dt);
+            ProduktDataGrid.ItemsSource = dt.DefaultView;
+            CloseConnection();
+        }
+    }
+}
